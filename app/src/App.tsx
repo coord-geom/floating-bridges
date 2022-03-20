21c0d965675f7b318865fbd5cd97fb246e932c5f
m0n0valu3nce <h1810035@nushigh.edu.sg>
2022-03-20 20:26:37 +0800

    tried to do some stuff for the webpage

--- app/src/App.tsx
+++ app/src/App.tsx
@@ -1,24 +1,42 @@
-import React from 'react';
 import logo from './logo.svg';
+import cards from './cards.gif';
 import './App.css';
+import React, { FC, useState } from "react"
+import ReactDOM from "react-dom"
+
+const suits = ["clubs", "diamonds", "hearts", "spades"]
+const numbers = ['2', '3', '4', '5', '6', '7', '8', '9', '10', 'jack', 'queen', 'king', 'ace']
+
+interface CardProps {
+  suit: number, //heart, dim, club, spade
+  num: number, //2 to Ace
+  hidden: boolean,
+  position?: number,
+  total?: number
+}
+
+export const Card: FC<CardProps> = (props) => {
+  const path = "./sprites/" + numbers[props.num] + "_of_" + suits[props.suit] + ".png"
+  console.log(encodeURI(path))
+  const cardName = numbers[props.num] + " of " + suits[props.suit]
+
+  const handleClick = () => {
+
+  }
+
+  return (
+    <button onClick={handleClick}>
+      <img src={require(encodeURI(path))} alt={cardName} className='card'/>
+    </button>
+  );
+  
+}
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Card suit={2} num={12} hidden={false}/>
     </div>
   );
 }
