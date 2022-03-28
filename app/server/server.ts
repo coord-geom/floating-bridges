943c9cd0b8edf39487dced9cf5fb43d92ad6e858
m0n0valu3nce <h1810035@nushigh.edu.sg>
2022-03-28 23:50:20 +0800

    server stuff. ALOT of server stuff

---
+++ app/server/server.ts
@@ -0,0 +1,30 @@
+import socketIO from 'socket.io'
+
+interface ServerToClientEvents {
+  noArg: () => void;
+  basicEmit: (a: number, b: string, c: Buffer) => void;
+  withAck: (d: string, callback: (e: number) => void) => void;
+}
+
+interface ClientToServerEvents {
+  hello: () => void;
+}
+
+interface InterServerEvents {
+  ping: () => void;
+}
+
+interface SocketData {
+  name: string;
+  age: number;
+}
+
+const io = new socketIO.Server<ClientToServerEvents, ServerToClientEvents, InterServerEvents, SocketData>(3000, {
+  cors:{
+    origin:["http://localhost:3001", "http://localhost:3000"]
+  }
+});
+
+io.on('connection', socket => {
+  console.log(socket.id)
+})
--- app/src/App.test.tsx
+++ app/src/App.test.tsx
@@ -1,9 +1,9 @@
 import React from 'react';
 import { render, screen } from '@testing-library/react';
-import App from './App';
+import AppRoom from './AppRoom';
 
 test('renders learn react link', () => {
-  render(<App />);
+  render(<AppRoom />);
   const linkElement = screen.getByText(/learn react/i);
   expect(linkElement).toBeInTheDocument();
 });
