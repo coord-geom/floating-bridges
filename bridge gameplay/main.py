736b1a41b0ae4f510816f8720232f6648072a781
coord-geom <ohzhiyuan1010@gmail.com>
2022-04-01 01:18:30 +0800

    more bug fixes

--- bridge gameplay/main.py
+++ bridge gameplay/main.py
@@ -5,8 +5,8 @@ import torch
 import os
 
 NUMGAMES    = 30000
-TIMERUN     = 1000
-PRINTCYCLE  = 1000
+TIMERUN     = 100
+PRINTCYCLE  = 100
 
 # code has been commented because it doesn't work
 
@@ -179,7 +179,7 @@ while (time.time()-true_start < TIMERUN): # game_cnt < NUMGAMES
 
     if game_cnt%PRINTCYCLE == 0:
         for agent in agents:
-            agents.train_long_memory()
+            agent.train_long_memory()
         print('Game Count:',game_cnt,'Time:',time.time()-start,'seconds\n')
         start = time.time()
 
@@ -192,12 +192,11 @@ mfp = 'model'
 if not os.path.exists(mfp):
     os.makedirs(mfp)
 
-for i in range(4):
-    for j in range(3):
-        m = agents[i][j].model
-        t = agents[i][j].trainer
-        torch.save({
-            'epoch': game_cnt/1000,
-            'model_state_dict': m.state_dict(),
-            'optimizer_state_dict': t.optimizer.state_dict()
-        }, 'model/AprilFoolsModel_P'+str(i)+'A'+str(j)+'.pth')
+for i in range(3):
+    m = agents[i].model
+    t = agents[i].trainer
+    torch.save({
+        'epoch': game_cnt/1000,
+        'model_state_dict': m.state_dict(),
+        'optimizer_state_dict': t.optimizer.state_dict()
+    }, 'model/AprilFoolsModel_Agent'+str(i)+'.pth')
